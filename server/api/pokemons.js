8e570f0c3d69264cb82aba26eb929b317472dc77
Carlos Oronoz <coronoz@ixpandit.com>
2021-02-26 19:32:52 -0300

    feat: add initial project structure and docker setup

---
+++ server/api/pokemons.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  console.log("POKEMONS");
+});
+
+module.exports = router;
---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const pokemons = require('./api/pokemons');
+
+
+app.use('/pokemons', pokemons);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
